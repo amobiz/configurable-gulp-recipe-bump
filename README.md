b30b330cf365d6faa29bc8a105596e0717263db8
amobiz <amobiz.tw+github@gmail.com>
2015-12-15 04:24:08 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# configurable-gulp-recipe-each-dir
+Configurable gulp recipe that bump project version.
---
+++ test/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var Sinon = require('sinon'),
+	Chai = require('chai'),
+	expect = Chai.expect;
+
+var base = process.cwd();
+
+var bump = require(base),
+	PluginError = require('gulp-util').PluginError;
+
+describe('bump()', function () {
+	it.skip('should ...', function () {
+	});
+});
+
